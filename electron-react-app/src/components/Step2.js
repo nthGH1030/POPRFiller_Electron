fff38627d217e1bb0e8879d8203299f7c1309e41
Kelvin Ngan <nth1030@gmail.com>
2024-05-30 07:32:05 +0800

    Addding a toggle switch

--- electron-react-app/src/components/Step2.js
+++ electron-react-app/src/components/Step2.js
@@ -2,12 +2,21 @@ import '../Styles.css';
 import React from 'react';
 import { Link } from "react-router-dom";
 import Nav from "./NavBar";
+import ToogleSwitch from "./ToogleSwitch";
 
 function Step2() {
     return (
         <>
         <Nav/>
             <h1>Step2</h1>
+
+        <p>Which template are you going to use?</p>
+        <ToogleSwitch label = "Templates"/>
+        
+
+        <p>Who is preparing this submission</p>
+        <input type = 'text' placeholder = 'John Doe, APM-PM'/>
+
         <Link to="/">
             <button type = 'button' className = "button">
                 Back
