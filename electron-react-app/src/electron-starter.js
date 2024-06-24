b6d86f8f24d73edeb71272d1ba4586cbd032f8e4
Kelvin Ngan <nth1030@gmail.com>
2024-06-25 06:54:04 +0800

    implemented wait-on so react sever can be run before electron sever start accessing it

--- electron-react-app/src/electron-starter.js
+++ electron-react-app/src/electron-starter.js
@@ -1,4 +1,65 @@
+const { app, BrowserWindow } = require('electron');
+const http = require('http');
+const parh = require('path');
 
+async function createWindow() {
+  const isDev = await import('electron-is-dev').then(module => module.default);
+
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const appURL = isDev
+    ? 'http://localhost:3000' // Development URL
+    : `file://${path.join(__dirname, '../build/index.html')}`; // Production path
+  mainWindow.loadURL(appURL).catch(err => console.error('Error loading the URL:', err));
+
+  /*
+  checkServerReady()
+    .then(() => {
+      mainWindow.loadURL('http://localhost:3000');
+    })
+    .catch(err => console.error('Server never became ready', err));
+    */
+
+  mainWindow.webContents.openDevTools()
+}
+
+function checkServerReady(attempt = 1) {
+  return new Promise((resolve, reject) => {
+    http.get('http://localhost:3000', (res) => {
+      if (res.statusCode === 200) {
+        console.log('Server is ready');
+        resolve();
+      } else {
+        console.log('Server responded, but not with a 200 status. Retrying...');
+        setTimeout(() => resolve(checkServerReady(attempt + 1)), 2000);
+      }
+    }).on('error', (err) => {
+      if (attempt > 5) { // Give up after 5 attempts
+        reject(new Error('Server not ready after 5 attempts'));
+      } else {
+        console.log(`Attempt ${attempt}: Server not ready, retrying...`);
+        setTimeout(() => resolve(checkServerReady(attempt + 1)), 2000);
+      }
+    });
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+
+/*
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
@@ -87,3 +148,4 @@ app.on('window-all-closed', () => {
 
 
 
+*/
