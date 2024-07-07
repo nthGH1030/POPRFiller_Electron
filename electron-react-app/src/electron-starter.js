888f2b425f28a438acfd93bb747faabae6372d74
Kelvin Ngan <nth1030@gmail.com>
2024-07-08 07:26:44 +0800

    Fixed issue when loading blank page in producition

--- electron-react-app/src/electron-starter.js
+++ electron-react-app/src/electron-starter.js
@@ -11,24 +11,11 @@ async function readFileContent(filePath) {
         return content;
     } catch (error) {
         console.error('Failed to read file:', error);
-        throw error; // Rethrow to handle it in the renderer process
+        throw error; 
     }
 }
 
-// Handle load-template-poii
-ipcMain.handle('load-template-po', async () => {
-  const poPath = path.join(__dirname, './secrets/template PO.xlsx');
-  console.log(poPath);
-  const PO = await readFileContent(poPath);
-  return PO;
-});
 
-// Handle load-template-pr
-ipcMain.handle('load-template-pr', async () => {
-  const prPath = path.join(__dirname, './secrets/template PR.xlsx');
-  const PR = await readFileContent(prPath);
-  return PR;
-});
 
 
 const createWindow = () => {
@@ -44,19 +31,39 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  const startUrl = process.env.WEB_URL || url({
-    pathname: path.join(__dirname, "../build/index.html"),
-    protocol: 'file',
-    slashes: true
-  })
-  //console.log(startUrl);
+  const startUrl = process.env.WEB_URL ||  `file://${path.join(__dirname, "../build/index.html")}`;
+
   mainWindow.loadURL(startUrl);
 
-  // Open the DevTools.
+  // Open the DevTools. 
   mainWindow.webContents.openDevTools();
 };
 
-//app.on('ready', createWindow);
+app.whenReady().then(() => {
+  // Handle load-template-poii
+  ipcMain.handle('load-template-po', async () => {
+    const poPath = path.join(__dirname, './secrets/template PO.xlsx');
+    console.log(poPath);
+    const PO = await readFileContent(poPath);
+    return PO;
+  });
+
+  // Handle load-template-pr
+  ipcMain.handle('load-template-pr', async () => {
+    const prPath = path.join(__dirname, './secrets/template PR.xlsx');
+    const PR = await readFileContent(prPath);
+    return PR;
+  });
+  createWindow()
+
+  app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+})
 
 
 app.on('window-all-closed', () => {
@@ -65,13 +72,5 @@ app.on('window-all-closed', () => {
   }
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
 
 
-app.whenReady().then(createWindow);
