d8d95765e16725690994a2836914dfbf13217dcb
Kelvin Ngan <nth1030@gmail.com>
2024-10-02 07:25:34 +0800

    halfway to implement writePO test and a function that read a writtenPO

--- electron-react-app/src/__tests__/writeFile.test.js
+++ electron-react-app/src/__tests__/writeFile.test.js
@@ -1,24 +1,65 @@
 import {extractDataFromExcel, readFile, readExcelFile} from '../utils/readFile.js';
+import {writePO} from '../utils/writeFile.js';
 import fs from 'fs';
 import path from 'path';
 
 //test for handle PO
 
 //test if data are indeed written in the correct cell
-//Write a template PO, check the file with readFile to verify the input
+//Write a template PO with a predefined data, read the PO and check if the input are written match
+
 /*
 describe('Test writePO to write correct data in excel' , () => {
-    let buffer
-    //extractedObj for testing purpose
+    let buffer;
+    let bufferArray;
+    let worksheet;
+    let row;
+    let data;
+    let templateWorksheet;
+
     beforeAll( async () => {
+        //extract data to ready to write into file
         const masterTablePath = path.join(__dirname, '../secrets/Master table_test.xlsx');
         buffer = await fs.promises.readFile(masterTablePath);
-        const testData = await extractDataFromExcel(buffer, 24)
+        bufferArray = toArrayBuffer(buffer);
+        worksheet = await readExcelFile(bufferArray, 'POPR summary');
+        row = 24;
+        data = await extractDataFromExcel(worksheet, row);
+
+        //write data into file
+        const templatePath = path.join(__dirname, '../secrets/text_template_PO.xlsx');
+        templateBuffer = await fs.promises.readFile(templatePath);
+        tempalteBufferArray = toArrayBuffer(templateBuffer);
+        templateWorksheet = await readExcelFile(tempalteBufferArray, 'PO_Input')
+        
+        const {filename,fileWritten} = await writePO(data, templateWorksheet)
+
+        //read the data in the file written
+        const fileWrittenBufferArray = toArrayBuffer(fileWritten);
+        const fileWrittenWorksheet = await readExcelFile(fileWrittenBufferArray, 'PO_Input')
+
     })
     
 
-    test('successfully write and save as an excel for row', async() => {
+    test('Write a PO with correct data', async() => {
         
+
+        const data = {
+            'Bundle': 'Project testing2',
+            'Entity': 'Test Location (Get Rich Quick Limited)',
+            'PO Number': 'TestingPoNumber2',
+            'Vendor': 'Get Rich Quick Limited',
+            'Type of expense': 'Capex',
+            'Capex Nature': 'Soft Cost',
+            'Purchase description / Payment Certification reason': 'Testing Purchase Description',
+            'Approved PO amount': 100000,
+            'PO Change Request': '',
+            'PO Change Request Date': '',
+            'Total Payment paid': 'N/A',
+            'Paid Requested': 'N/A',
+            'Delivery date': 'N/A',
+            'Invoice number': 'N/A'
+        }
     })
 })
-    */
+*/
