73388984a5f6c259545f773d0c4ab22b600ddef7
Kelvin Ngan <nth1030@gmail.com>
2024-10-09 10:46:54 +0800

    adjust style for sidebar to prevent it from shrinking

--- electron-react-app/src/components/replaceTemplate.js
+++ electron-react-app/src/components/replaceTemplate.js
@@ -1,11 +1,22 @@
 import '../Styles.css';
 import React from 'react';
+import SideNavBar from './sideNavBar';
 
 const ReplaceTemplate = () => {
 
     return (
-        <div>
-            <p>This is replaceTemplate</p>
+        <div className = 'page'>
+            <div className = 'sidebar-container'>
+                <SideNavBar>
+                </SideNavBar>
+            </div>
+            <div className = 'replaceTemplate-container'>
+                <p>This is where the template should be replaced</p>
+
+            </div>
+
+
+
         </div>
     )
 } 
