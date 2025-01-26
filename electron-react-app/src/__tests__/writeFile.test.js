d2781eb4ba0614f1fb38902c648885eb74283869
nthGH1030 <nth1030@gmail.com>
2025-01-26 11:15:13 +0800

    complet test for writePOPR in PO

--- electron-react-app/src/__tests__/writeFile.test.js
+++ electron-react-app/src/__tests__/writeFile.test.js
@@ -1,5 +1,5 @@
 import {extractDataFromExcel, readFile, readExcelFile, extractDataFromPOPR} from '../utils/readFile.js';
-import {writePO, writePR} from '../utils/writeFile.js';
+import {writePOPR} from '../utils/writeFile.js';
 import { toArrayBuffer } from '../utils/Parser_toArrayBuffer.js';
 import fs from 'fs';
 import path from 'path';
@@ -9,26 +9,51 @@ import path from 'path';
 //test if data are indeed written in the correct cell
 //Write a template PO with a predefined data, read the PO and check if the input are written match
 
-/*
-describe('Test writePO to write correct data in excel' , () => {
+
+describe('Test writePOPR to write correct data in PO' , () => {
     let writtenData
+    let staff;
 
     beforeAll( async () => {
+
+        // Mock localStorage for the value of 'staff'
+        const localStorageMock = (() => {
+        let localStorage = {};
+        return {
+          getItem: jest.fn((key) => localStorage[key] || null),
+          setItem: jest.fn((key, value) => {
+            localStorage[key] = value.toString();
+          }),
+          removeItem: jest.fn((key) => {
+            delete localStorage[key];
+          }),
+          clear: jest.fn(() => {
+            localStorage = {};
+          })
+        };
+      })();
+
+        Object.defineProperty(global, 'localStorage', { value: localStorageMock });
+        localStorage.setItem('staff', 'John Doe');
+        staff = localStorage.getItem('staff');
+        console.log('localStorage staff:', localStorage.getItem('staff'));
+
         //extract data to ready to write into file
-        const masterTablePath = path.join(__dirname, '../secrets/Master table_test.xlsx');
-        const buffer = await fs.promises.readFile(masterTablePath);
+        const centralTablePath = path.join(__dirname, '../demoTemplate/Central record_demo.xlsx');
+        const buffer = await fs.promises.readFile(centralTablePath);
         const bufferArray = toArrayBuffer(buffer);
         const worksheet = await readExcelFile(bufferArray, 'POPR summary');
-        const row = 24;
-        const data = await extractDataFromExcel(worksheet, row);
+        const row = 5;
+        const extractedObj = await extractDataFromExcel(worksheet, row);
 
         //write data into file
-        const templatePath = path.join(__dirname, '../secrets/test_template_PO.xlsx');
+        const templatePath = path.join(__dirname, '../demoTemplate/template PO_demo.xlsx');
         const templateBuffer = await fs.promises.readFile(templatePath);
         const tempalteBufferArray = toArrayBuffer(templateBuffer);
         const templateWorksheet = await readExcelFile(tempalteBufferArray, 'PO_Input')
         
-        const {filename, buffer: fileWritten} = await writePO(data, templateWorksheet)
+        const {filename: outputFilename, buffer: fileWritten} = 
+            await writePOPR(extractedObj, templateWorksheet, 'PO')
 
         //read the data in the file written
         const fileWrittenBufferArray = toArrayBuffer(fileWritten);
@@ -36,27 +61,39 @@ describe('Test writePO to write correct data in excel' , () => {
         writtenData = await extractDataFromPOPR(fileWrittenWorksheet)
 
     })
-    
 
+    afterAll(() => {
+        // Restore the original localStorage implementation
+        jest.restoreAllMocks();
+      });
+    
+    
     test('Write a PO with correct data', async() => {
-        
+        console.log('Test staff:', staff); // Debugging statement
+        const date = new Date('2025-01-20');
         const matchData = {
-            'PO Number': 'TestingPoNumber2',
-            'Entity': 'Test Venue (Get Rich Quick Limited)',
-            'Description of purchase': 'Testing Purchase Description',
-            'Type of expenses': 'Capex',
+            '#Key_Row': "",
+            'Bundle': 'Project X',
+            'Entity': 'Get Rich Fast Limited',
+            'PO Number': 'A0123456789',
+            'Vendor': 'PaymeNow Limited',
+            'Type of expense': 'Capex',
             'Capex Nature': 'Soft Cost',
-            'Amount': 100000,
-            'Vendor': 'Get Rich Quick Limited',
-            'Staff': '',
-            'Change': '',
-            'Change Date': ''
+            'Purchase description / Payment Certification reason': 'Additional Consultancy service from GetRich Fast Limited',
+            'Approved PO amount': 150000,
+            'PO Change Request': 50000,
+            'PO Change Request Date': date,
+            'Total Payment paid': '',
+            'Paid Requested': 'N/A',
+            'Delivery date': 'N/A',
+            'Invoice number': 'N/A',
+            'staff': staff,
         }
 
         await expect(Promise.resolve(writtenData)).resolves.toStrictEqual(matchData)
     })
 })
-
+/*
 describe('Test writePR to write correct data in excel', () => {
     let writtenData
 
@@ -105,6 +142,6 @@ describe('Test writePR to write correct data in excel', () => {
         await expect(Promise.resolve(writtenData)).resolves.toStrictEqual(matchData)
 
      })
+    
 })
-
 */
