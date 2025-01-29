389089cd6e94e17e521a7fb639c152fd1947a7c4
nthGH1030 <nth1030@gmail.com>
2025-01-29 12:49:00 +0800

    database is not created when directory is not required to be created

--- electron-react-app/src/__tests__/replaceFile.test.js
+++ electron-react-app/src/__tests__/replaceFile.test.js
@@ -1,5 +1,10 @@
 const fs = require ('fs').promises;
-import path from 'path'
+const path = require('path');
+const {app} = require('electron');
+const { ensureDatabaseExist, parseFile, appendtoDatabase, 
+    updateDatabase, checkForDuplicate, getUserConfirmation, ensureTemplateDirectoryExist,
+    saveTemplates, getFileDatabyTemplateType, selectAndDeselectTemplate, deselectAllTemplate,
+    findTemplate, findSelectedTemplate } = require('../utils/manageUserData');
 //This unit test check if the upload new template and select new template works correctly
 
 /*
@@ -9,9 +14,23 @@ import path from 'path'
     test4: check if you can selecting a new template works correctly
 */
 
+jest.mock('../utils/manageUserData' , () => {
+    const originalModule = jest.requireActual('../utils/manageUserData');
+    return {
+        ...originalModule,
+        saveTemplates: jest.fn(),
+    };
+})
+
+
 describe( 'Test upload a new template function', () => {
     const testPath = path.join(__dirname, 'testDirectory')
-    const testFilePath = path.join(testPath, 'userDatabase.json');
+    const testDatabasePath = path.join(testPath, 'userDatabase.json');
+
+    const mockTemplateType = 'mockTemplateType';
+    const mockFilename = 'mockFile.xlsx';
+    const mockFileBufferArray = [1, 2, 3, 4];
+    const mockTestFilePath = path.join(testPath, mockFilename);
     
     beforeAll(async() => {
 
@@ -20,26 +39,39 @@ describe( 'Test upload a new template function', () => {
         } catch (err) {
          if (err.code === 'ENOENT') {
              await fs.mkdir(testPath, {recursive: false})
-             await fs.writeFile(testFilePath , '[]')
          }
-        }
+        } 
+        await fs.writeFile(testDatabasePath , '[]')
     })
-
+    
     afterAll(async() => {
         try {
-            await fs.rm(testFilePath);
+            await fs.rm(testDatabasePath, { force: true });
+            await fs.rm(mockTestFilePath, { force: true });
             
         } catch (err) {
-            console.log('faile to remove scaffold',err)
+            console.log('failed to remove scaffold',err)
         }
     })
 
-
-    test('If an uploaded template is saved to correct directory' , () => {
+    test('If an uploaded template is saved to correct directory' , async() => {
         /*
             i can use file system from nodejs here
-            but how do i mock a file system without actually creating a file and check
+            check if saveTemplate works
         */
+        //await saveTemplates('mocked array buffer', filename, templateType) 
+        saveTemplates.mockImplementation(async(fileBufferArray, filename, templateType)=> {
+            const buffer = Buffer.from(fileBufferArray)
+            const testFilePath = path.join(testPath, filename)
+            await fs.writeFile(testFilePath, buffer)
+            return {success : true, testFilePath : testFilePath}
+        })
+
+        const result = await saveTemplates(mockFileBufferArray, mockFilename, mockTemplateType)
+        const fileExists = await fs.access(result.testFilePath).then(() => true).catch(() => false);
+
+        expect(result. success).toEqual(true)
+        expect(fileExists).toBe(true);
 
     })
     test('if the database is created and can be accessed' , () => {
