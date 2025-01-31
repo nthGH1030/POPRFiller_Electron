e2cb185dff3e49ac016b9e71e3f24b158c35ddd9
nthGH1030 <nth1030@gmail.com>
2025-01-31 12:49:09 +0800

    update findTempalte test

--- electron-react-app/src/__tests__/replaceFile.test.js
+++ electron-react-app/src/__tests__/replaceFile.test.js
@@ -14,6 +14,7 @@ const { ensureDatabaseExist, getDatabaseAsObj,parseFile, appendtoDatabase,
     test4: check if you can selecting a new template works correctly
 */
 
+
 jest.mock('../utils/manageUserData' , () => {
     const originalModule = jest.requireActual('../utils/manageUserData');
     
@@ -23,21 +24,17 @@ jest.mock('../utils/manageUserData' , () => {
         appendtoDatabase: jest.fn(),
         getDatabaseAsObj: jest.fn(),
         updateDatabase: jest.fn(),
+        findTemplate: jest.fn(),
     };
 })
-
-
 
 describe( 'Test upload a new template function', () => {
     const testPath = path.join(__dirname, 'testDirectory')
     const testDatabasePath = path.join(testPath, 'userDatabase.json');
-
     const mockTemplateType = 'mockTemplateType';
     const mockFilename = 'mockFile.xlsx';
     const mockFileBufferArray = [1, 2, 3, 4];
     const mockTestFilePath = path.join(testPath, mockFilename);
-
-
 
     beforeEach(async() => {
         try {
@@ -71,12 +68,8 @@ describe( 'Test upload a new template function', () => {
         }
     })
 
-    test('If an uploaded template is could be saved to a directory' , async() => {
-        /*
-            i can use file system from nodejs here
-            check if saveTemplate works
-        */
-
+    test('saveTemplates' , async() => {
+        
         saveTemplates.mockImplementation(async(fileBufferArray, filename, templateType)=> {
             try {
                 const buffer = Buffer.from(fileBufferArray)
@@ -88,7 +81,7 @@ describe( 'Test upload a new template function', () => {
             }
             
         })
-
+        
         const result = await saveTemplates(mockFileBufferArray, mockFilename, mockTemplateType)
         const fileExists = await fs.access(result.testFilePath).then(() => true).catch(() => false);
 
@@ -96,14 +89,9 @@ describe( 'Test upload a new template function', () => {
         expect(fileExists).toBe(true);
 
     })
-
+    
     test('if the database is created, accessed and parsed as object' , async() => {
-        /*
-            see if database directory is created
-            see if database is saved to the directory
-            see if database can be accessed
-            see if the database content can be parsed
-        */
+
         const fileExists = await fs.access(testDatabasePath).then(()=> true).catch(()=> false)
         const databaseObj = await parseFile("template PO_test.xlsx", "PO")
 
@@ -115,12 +103,9 @@ describe( 'Test upload a new template function', () => {
             "status": "unselected"
         })
     })
+    
     test('if the database is appended correctly' , async() => {
-        /*
-            Make a data entry
-            append the data entry to the database
-            get the database to check if the mock resolved value is correct
-        */
+
         const mockEntry = {
             "filename": "template PO_test_newEntry.xlsx",
             "uploadDate": 1737687395921,
@@ -173,12 +158,6 @@ describe( 'Test upload a new template function', () => {
     })
     
     test('if the database is updated correctly in case of duplicated entry', async() => {
-        /*
-            get the database as object
-            update the database entry with the new one
-            write the database
-            check if the updated database is as expected
-        */
        
         const updatedDataObj = 
         [
@@ -221,12 +200,53 @@ describe( 'Test upload a new template function', () => {
                 "status": "selected"
             }
         ])
-            
     })
-})
 
-describe('Test select new template function',() => {
-    test('When a template is clicked the database is updated and new template is selected' , () => {
+    test('find template' , async() => {
+        /*
+            check that a template can be found via its filename property
+            check that a template cna be found via 'selected' property
+        */
+
+        getDatabaseAsObj.mockImplementation(async() => {
+            const databaseFilePath = path.join(__dirname, './testDirectory/userDatabase.json')
+            const databaseBuffer = await fs.readFile(databaseFilePath);
+            const databaseObj = JSON.parse(databaseBuffer);
+            return databaseObj
+        })
+
+       findTemplate.mockImplementation(async(filename)=> {
+            const databaseObj = await getDatabaseAsObj()
+            const requestedObj = databaseObj.find(entry => entry.filename === filename)
+            if (requestedObj) {
+        
+                return requestedObj
+        } 
+       })
+        const result = await findTemplate('template PO_test.xlsx')
+        expect(result).toEqual({
+            "filename": "template PO_test.xlsx",
+            "uploadDate": 1737687395921,
+            "templateType": "PO",
+            "status": "unselected"
+        })
+    })
+
+    
+    test('selectAndDeselectTemplate' , async() => {
+        /*
+            When a template is selected, check that no other template is selected
+        */
 
     })
+
+    test('deselectAllTempalte', () => {
+        /*
+            Check that all temlplates are deselected
+        */
+    })
+
+
+
+    
 })
