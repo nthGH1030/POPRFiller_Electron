15d0f4b2afefab44deea72a93c1658f60fee5fb1
nthGH1030 <nth1030@gmail.com>
2025-02-23 17:36:18 +0800

    tweaking styles of tempalte History

--- electron-react-app/src/components/template.js
+++ electron-react-app/src/components/template.js
@@ -4,9 +4,8 @@ import '../Styles.css';
 const Template = ({filename, uploadDate, selected, onClick}) => {
     return (
         
-        <div className = 'template-container'>
-            <div className = {`highlight-if-selected 
-                ${selected === 'selected' ? 'selected': ''}`}>
+        <div className = {`highlight-if-selected 
+            ${selected === 'selected' ? 'selected': ''}`}>
                 <span 
                     className = 'file-info' 
                     onClick = {onClick}>
@@ -14,8 +13,9 @@ const Template = ({filename, uploadDate, selected, onClick}) => {
                     <br />
                     Upload Date: {new Date(uploadDate).toLocaleDateString()}
                 </span>
-            </div>
-            <span className = 'selected-status'>
+            
+            <span className = {`selected-status 
+                ${selected === 'selected'? 'selected' : ''}`}>
                 {selected}
             </span>
         </div>
