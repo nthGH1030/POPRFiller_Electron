6f85071ffd213822656f06f7578d8fc350e074f8
nthGH1030 <nth1030@gmail.com>
2025-03-13 11:31:41 +0800

    implemented conditional highlight for linktofeature

--- electron-react-app/src/components/linkToFeature.js
+++ electron-react-app/src/components/linkToFeature.js
@@ -3,13 +3,12 @@ import '../Styles.css';
 import { Link } from "react-router-dom";
 
 const LinkToFeature = ({route, imagePath, currentRoute, tooltip}) => {
-
-    let divContainerClass = "link-To-Feature-Container"
-
-    divContainerClass += currentRoute === route ? " Active" : "";
     
     return (
-    <div className = {divContainerClass} data-tooltip={tooltip}>
+    <div className = 
+        {currentRoute === route ? 'link-To-Feature-Container': 
+            'link-To-Feature-Container Inactive'}
+            data-tooltip={tooltip}>
         <Link to = {route}>
             <div className = 'link-To-Feature-Description'>
                 <img 
